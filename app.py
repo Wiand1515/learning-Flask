b3833e7e3e4ecaf9bae6909252fa797c81b23385
Wiand1515 <marofual@gmail.com>
2021-07-04 23:30:16 -0400

    Consulta metodo get

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 from flask import Flask
 
 app = Flask( __name__ )
+
+@app.route('/')
+def main():
+    return "Hola desde flask"
+
+@app.route('/method/get', methods=['GET'])
+def query_method_get():
+    return 'Consultando con GET'
+
+if __name__ == '__main__':
+    app.run()
+
