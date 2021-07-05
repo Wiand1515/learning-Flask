328ac208c8ea1d77c1d791b82647166f83594ba9
Wiand1515 <marofual@gmail.com>
2021-07-04 23:39:18 -0400

    Todos los metodos

--- app.py
+++ app.py
@@ -10,6 +10,22 @@ def main():
 def query_method_get():
     return 'Consultando con GET'
 
+@app.route('/method/post', methods=['POST'])
+def query_method_post():
+    return 'Consultando con POST'
+
+@app.route('/method/put', methods=['PUT'])
+def query_method_put():
+    return 'Consultando con PUT'
+
+@app.route('/method/delete', methods=['DELETE'])
+def query_method_delete():
+    return 'Consultando con DELETE'
+
+@app.route('/method/all', methods=['GET','POST','PUT','DELETE'])
+def query_method_all():
+    return 'Consultando con todos los metodos'
+
 if __name__ == '__main__':
     app.run()
 
